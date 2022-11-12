c01f9bece621f968a9a05e5439a6c708fcf4b304
Nazia Sayyad <sayyadnazia257@gmai.com>
2022-11-12 21:21:05 +0530

    done

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const express = require("express");
 const cros = require("cors");
 const connect = require("./config/db");
 const userRoute = require("./features/User/user.route");
+const moongose = require("mongoose");
+
+moongose.connect(process.env.DB_URL);
+moongose.connect("mongodb://127.0.0.1:27017/nazia",{
+    useNewUrlParser: "true",
+  useUnifiedTopology: "true"
+})
+moongose.connection.on("error",err =>{
+    console.log("err", err);
+})
+moongose.connection.on("connected", (err,res)=>{
+    console.log("mongoose is connected");
+});
 
 const app = express();
 app.use(express.json());
